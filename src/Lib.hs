c383d02fb72a426a7365f17708601e39c6726253
charltonaustin <charlton.austin@gmail.com>
2024-10-24 07:43:39 -0700

    Learning from https://learn-haskell.blog/03-html/02-type_signatures.html

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,19 +3,36 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn (makeHtml "My page title" "My page content")
+someFunc = putStrLn myhtml
+
+myhtml :: String
+myhtml =
+  makeHtml
+    "Hello title"
+    (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
 html_ :: String -> String
-html_ content = "<html>" <> content <> "</html>"
+html_ = el "html"
 
 body_ :: String -> String
-body_ content = "<body>" <> content <> "</body>"
+body_ = el "body"
 
 title_ :: String -> String
-title_ content = "<title>" <> content <> "</title>"
+title_ = el "title"
 
 head_ :: String -> String
-head_ content = "<head>" <> content <> "</head>"
+head_  = el "head"
+
+p_ :: String -> String
+p_  = el "p"
+
+h1_ :: String -> String
+h1_  = el "h1"
+
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
