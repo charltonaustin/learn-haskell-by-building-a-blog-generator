4a1d1f41b7606c5668f8e4444598fe58ad0d96c5
charltonaustin <charlton.austin@gmail.com>
2024-10-27 06:58:02 -0700

    Learning from https://learn-haskell.blog/03-html/06-escaping_characters.html

--- src/Html.hs
+++ src/Html.hs
@@ -1,35 +1,62 @@
 module Html
-   ( Html
-   , Title
-   , Structure
-   , html_
-   , p_
-   , h1_
-   , append_
-   , render
-   ) where
+  ( Html,
+    Title,
+    Structure,
+    html_,
+    p_,
+    h1_,
+    append_,
+    render,
+  )
+where
+
+-- * Types
 
 newtype Html = Html String
+
 newtype Structure = Structure String
+
 type Title = String
 
+-- * EDSL
+
+html_ :: Title -> Structure -> Html
+html_ title (Structure content) = Html (el "html" (el "head" (el "title" (escape title)) <> el "body" content))
+
+p_ :: String -> Structure
+p_ = Structure . el "p" . escape
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1" . escape
 
 append_ :: Structure -> Structure -> Structure
-append_ (Structure x) (Structure y) = Structure (x <> y)
+append_ c1 c2 =
+  Structure (getStructureString c1 <> getStructureString c2)
+
+-- * Render
 
 render :: Html -> String
 render (Html s) = s
+
+-- * Utilities
 
 el :: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-p_ :: String -> Structure
-p_  = Structure . el "p"
+getStructureString :: Structure -> String
+getStructureString content =
+  case content of
+    Structure str -> str
 
-h1_ :: String -> Structure
-h1_  = Structure . el "h1"
-
-
-html_ :: Title -> Structure -> Html
-html_ title (Structure content) = Html (el "html" (el "head" (el "title" title) <> el "body" content))
+escape :: String -> String
+escape =
+  let escapeChar c =
+        case c of
+          '<' -> "&lt;"
+          '>' -> "&gt;"
+          '&' -> "&amp;"
+          '"' -> "&quot;"
+          '\'' -> "&#39;"
+          _ -> [c]
+   in concat . map escapeChar
