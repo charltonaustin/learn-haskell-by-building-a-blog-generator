9861f12488931988b0e7b6dc8047139adbfddc51
charltonaustin <charlton.austin@gmail.com>
2024-10-30 17:13:39 -0700

    Learning from https://learn-haskell.blog/04-markup/02-parsing_01.html

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,21 @@
 import Html
+import Markup (parse, renderMarkup)
 
 main :: IO ()
-main = putStrLn (render myhtml)
+main = do
+    putStrLn (render myhtml)
+    putStrLn (renderMarkup (parse file))
 
 myhtml :: Html
 myhtml =
   html_
     "Hello title"
     (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
+
+file :: String
+file =
+  unlines
+    [ "first paragraph",
+      "",
+      "second paragraph"
+    ]
--- src/Markup.hs
+++ src/Markup.hs
@@ -1,6 +1,8 @@
 module Markup (
     Document
     , Structure(..)
+    , parse
+    , renderMarkup
 ) where
 
 import Numeric.Natural
@@ -14,3 +16,35 @@ data Structure
   | OrderedList [String]
   | CodeBlock [String]
   deriving Show
+
+renderMarkup :: Document -> String
+renderMarkup s =
+    case s of
+        [] -> ""
+        d:rest ->
+            case d of
+                Heading n s -> s <> renderMarkup rest
+                Paragraph s -> s <> renderMarkup rest
+                UnorderedList xs -> concat xs <>  renderMarkup rest
+                OrderedList xs -> concat xs <> renderMarkup rest
+                CodeBlock xs -> concat xs <> renderMarkup rest
+
+parse :: String -> Document
+parse = parseLines [] . lines
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts =
+  let
+    paragraph = Paragraph (unlines (reverse currentParagraph))
+  in
+    case txts of
+      [] -> [paragraph]
+      currentLine : rest ->
+        if trim currentLine == ""
+          then
+            paragraph : parseLines [] rest
+          else
+            parseLines (currentLine : currentParagraph) rest
+
+trim :: String -> String
+trim = unwords . words
