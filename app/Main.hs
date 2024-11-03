7a8df99a8ed57a93469ad8075a6e879d5732a359
charltonaustin <charlton.austin@gmail.com>
2024-11-03 14:35:57 -0800

    Learning from https://learn-haskell.blog/05-glue/02-io.html

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 -- Main.hs
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use lambda-case" #-}
 module Main where
 
+import Control.Monad (when)
 import Convert (convert)
 import qualified Html
 import qualified Markup
@@ -11,19 +15,16 @@ main :: IO ()
 main =
   getArgs >>= \args ->
     case args of
-      -- No program arguments: reading from stdin and writing to stdout
-      [] ->
-        getContents >>= \content ->
-          putStrLn (process "Empty title" content)
-      -- With input and output file paths as program arguments
-      [input, output] ->
-        readFile input >>= \content ->
-          doesFileExist output >>= \exists ->
-            let writeResult = writeFile output (process input content)
-             in if exists
-                  then whenIO confirm writeResult
-                  else writeResult
-      -- Any other kind of program arguments
+      [] -> do
+        content <- getContents
+        putStrLn (process "Empty title" content)
+      [input, output] -> do
+        content <- readFile input
+        exists <- doesFileExist output
+        let writeResult = writeFile output (process input content)
+         in if exists
+              then whenIO confirm writeResult
+              else writeResult
       _ ->
         putStrLn "Usage: learn-haskell-by-building-a-blog-generator-exe <input-file> <output-file>"
 
@@ -31,20 +32,17 @@ process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
 
 confirm :: IO Bool
-confirm =
+confirm = do
   putStrLn "Are you sure? (y/n)"
-    *> getLine
-    >>= \answer ->
-      case answer of
-        "y" -> pure True
-        "n" -> pure False
-        _ ->
-          putStrLn "Invalid response. use y or n"
-            *> confirm
+  answer <- getLine
+  case answer of
+    "y" -> pure True
+    "n" -> pure False
+    _ -> do
+      putStrLn "Invalid response. use y or n"
+      confirm
 
 whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action =
-  cond >>= \result ->
-    if result
-      then action
-      else pure ()
+whenIO cond action = do
+  result <- cond
+  when result action
